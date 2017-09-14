72a91e355abc1de57a018240fad4fe8933478bb4
Yao Yuan <opal_obsidian@hotmail.com>
2017-09-15 00:12:17 +0100

    imported ByteArrayOutputStream for testing command line output

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,7 +2,10 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
+    String[] books;
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+        System.out.println("Welcome to Biblioteca");
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -2,12 +2,31 @@ package com.twu.biblioteca;
 
 
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import static org.junit.Assert.assertEquals;
+
 
 public class BibliotecaAppTest {
 
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+    }
+
     @Test
     public void testWelcomeMessage() {
+
         assertEquals(1, 1);
     }
 
