469d10441f0856ff37c38865e7de0067113c00c4
Yao Yuan <opal_obsidian@hotmail.com>
2017-09-14 16:33:20 +0100

    added list of skeleton test cases

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -4,10 +4,57 @@ package com.twu.biblioteca;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-public class ExampleTest {
+public class BibliotecaAppTest {
 
     @Test
-    public void test() {
+    public void testWelcomeMessage() {
+        assertEquals(1, 1);
+    }
+
+
+    @Test
+    public void testListBooks() {
+        assertEquals(1, 1);
+    }
+
+
+    @Test
+    public void testBookDetails() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void testMainMenu() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void testInvalidMenuOption() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void testQuit() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void testSuccessfulCheckout() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void testUnsuccessfulCheckout() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void testSucessfulReturn() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void testUnsucessfulReturn() {
         assertEquals(1, 1);
     }
 }
